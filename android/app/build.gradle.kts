58a7569714075d81eefb244ef9e8a48c741b9094
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-07-02 01:33:28 +0800

    feat / chores: enhance notification support and permissions for Android and Windows

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,12 +7,15 @@ plugins {
 
 android {
     namespace = "com.example.study_forge"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
+        // FOR NOTIFICATIONS
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+
     }
 
     kotlinOptions {
@@ -20,12 +23,14 @@ android {
     }
 
     defaultConfig {
+        // THIS IS FOR THE ANDROID NOTIFICATION SUPPORT
+        multiDexEnabled = true
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.study_forge"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -38,6 +43,15 @@ android {
         }
     }
 }
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+
+    // FOR SAID WINDOW BUG
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
+}
+
 
 flutter {
     source = "../.."
