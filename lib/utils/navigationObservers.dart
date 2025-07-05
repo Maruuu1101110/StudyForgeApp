4f6af418a4bfcf4a29aa52eee76722c8d0d909dc
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-07-05 20:40:00 +0800

    feat: integrate NavigationSource enum into NotificationService for improved navigation handling

--- lib/utils/navigationObservers.dart
+++ lib/utils/navigationObservers.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
+
+enum NavigationSource { sidebar, homePage, direct }
