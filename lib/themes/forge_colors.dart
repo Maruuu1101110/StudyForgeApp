9defc757fa3a42eb84c1faace0b38ff6576163cb
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-07-09 21:52:55 +0800

    feat: add ForgeColors class for centralized color management in the app

---
+++ lib/themes/forge_colors.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ForgeColors {
+  // 🖤 Backgrounds & Layouts
+  static const Color scaffoldBackground = Color.fromARGB(255, 15, 15, 15);
+  static const Color transparent = Colors.transparent;
+  static const Color silverGray = Color(0xFFC0C0C0);
+  static const Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color black20 = Colors.black.withOpacity(0.2);
+  static Color black40 = Colors.black.withOpacity(0.4);
+
+  // 🤍 Text & Headers
+  static const Color headerText = Colors.white;
+  static Color hintText = Colors.white.withOpacity(0.6);
+  static Color inactiveText = Colors.white.withOpacity(0.4);
+  static Color inputBorderColor = Colors.white.withOpacity(0.2);
+
+  // 📎 Input Fields
+  static Color errorBorder = Colors.red.withOpacity(0.5);
+  static Color attachedFileBg = Colors.grey.shade800.withOpacity(0.2);
+
+  // 🌞 Accent & Highlights
+  static const Color amber = Colors.amber;
+  static Color amber100 = Colors.amber.shade100;
+  static Color amber200 = Colors.amber.shade200;
+  static Color amber300 = Colors.amber.shade300;
+  static Color amber600 = Colors.amber.shade600;
+
+  static Color orange = Colors.orange;
+  static Color orange200 = Colors.orange.shade200;
+  static Color orange300 = Colors.orange.shade300;
+  static Color orange500 = Colors.orange.shade500;
+  static Color orange600 = Colors.orange.shade600;
+  static Color orange700 = Colors.orange.shade700;
+
+  // 🎨 Buttons & FABs
+  static const Color green = Colors.green;
+  static const Color blue = Colors.blue;
+  static Color purple = Colors.purple;
+  static Color purple600 = Colors.purple.shade600;
+  static Color pink600 = Colors.pink.shade600;
+  static const Color deepOrange = Colors.deepOrange;
+}
