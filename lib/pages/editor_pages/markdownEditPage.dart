99f220dac4609d73f4e6fe71da47f8964583e3fc
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-07-10 20:56:03 +0800

    feat: refactor MarkDownEditPage for improved state management and code organization

--- lib/pages/editor_pages/markdownEditPage.dart
+++ lib/pages/editor_pages/markdownEditPage.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-// custom widgets
+import 'package:markdown_widget/markdown_widget.dart';
 import 'package:study_forge/components/markdownShortcut.dart';
 import 'package:study_forge/components/wordLimiter.dart';
-
-// database
 import 'package:study_forge/tables/note_table.dart';
 import 'package:study_forge/components/smart_md_editor.dart';
-
-//markdown packages //
-import 'package:markdown_widget/markdown_widget.dart';
+import 'package:study_forge/utils/file_manager_service.dart';
+import 'package:path/path.dart' as path;
+import 'package:study_forge/utils/code_wrapper.dart';
 
 class MarkDownEditPage extends StatefulWidget {
   final NoteManager noteManager;
@@ -34,135 +32,197 @@ class MarkDownEditPage extends StatefulWidget {
 }
 
 class _MarkDownEditPageState extends State<MarkDownEditPage> {
-  final noteManager = NoteManager();
-  late TextEditingController _titleController = TextEditingController();
-  late TextEditingController _contentController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  bool isOnRead = true;
+  bool isSaveEnabled = false;
+
+  final codeWrapper = (Widget child, String text, String language) =>
+      CodeWrapperWidget(child: child, text: text);
 
   @override
   void initState() {
     super.initState();
-    _titleController = TextEditingController(text: widget.title);
-    _contentController = TextEditingController(text: widget.content);
-
-    _titleController.addListener(() {
-      setState(() {});
-    });
+    _titleController.text = widget.title ?? '';
+    _contentController.text = widget.content ?? '';
 
     _titleController.addListener(_updateSaveButtonState);
     _contentController.addListener(_updateSaveButtonState);
   }
 
-  bool isSaveEnabled = false;
   void _updateSaveButtonState() {
     final hasText =
         _titleController.text.trim().isNotEmpty ||
         _contentController.text.trim().isNotEmpty;
-    if (hasText != isSaveEnabled) {
-      setState(() => isSaveEnabled = hasText);
+    setState(() => isSaveEnabled = hasText);
+  }
+
+  void _addOrUpdateNote() async {
+    final id = widget.id ?? Uuid().v4();
+    final title = _titleController.text.trim();
+    final content = _contentController.text.trim();
+
+    if (widget.id != null) {
+      await widget.noteManager.updateNote(id, title, content, widget.isMD);
+    } else {
+      await widget.noteManager.addNote(id, title, content, widget.isMD);
     }
+
+    Navigator.pop(context);
   }
 
-  bool isOnRead = true;
+  Future<String?> _showSubjectPicker() async {
+    final basePath = await FileManagerService.instance.getStudyForgeBasePath();
+    final roomDirs = Directory(basePath).listSync().whereType<Directory>();
 
-  @override
-  void dispose() {
-    _titleController.dispose();
-    _contentController.dispose();
-    super.dispose();
+    final subjects = <String>[];
+    for (final dir in roomDirs) {
+      final metadata = await FileManagerService.instance
+          .getRoomMetadataFromPath(dir.path);
+      if (metadata != null && metadata['subject'] != null) {
+        subjects.add(metadata['subject']);
+      }
+    }
+
+    return await showDialog<String>(
+      context: context,
+      builder: (context) {
+        final theme = Theme.of(context);
+        return AlertDialog(
+          backgroundColor: const Color(0xFF1E1E1E),
+          title: Row(
+            children: const [
+              Icon(Icons.school, color: Colors.amber),
+              SizedBox(width: 10),
+              Text('Pick a Subject', style: TextStyle(fontFamily: "Petrona")),
+            ],
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          content: Container(
+            constraints: const BoxConstraints(maxHeight: 300, minWidth: 280),
+            width: double.minPositive,
+            child: subjects.isEmpty
+                ? const Center(child: Text("No subjects found."))
+                : ListView.separated(
+                    itemCount: subjects.length,
+                    separatorBuilder: (_, __) => const Divider(height: 1),
+                    itemBuilder: (context, index) {
+                      final subject = subjects[index];
+                      return ListTile(
+                        leading: const Icon(
+                          Icons.book_outlined,
+                          color: Colors.deepOrangeAccent,
+                        ),
+                        title: Text(
+                          subject,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onTap: () => Navigator.of(context).pop(subject),
+                      );
+                    },
+                  ),
+          ),
+        );
+      },
+    );
   }
 
-  void addNote() {
-    final idToUse = widget.id ?? Uuid().v4();
-    widget.noteManager.addNote(
-      idToUse,
-      _titleController.text,
-      _contentController.text,
-      widget.isMD,
+  Future<void> _exportMarkdown() async {
+    final markdownText = _contentController.text;
+    if (markdownText.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Note is empty!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    final selectedSubject = await _showSubjectPicker();
+    if (selectedSubject == null) return;
+
+    final matchedRoomId = await FileManagerService.instance
+        .getRoomIdFromSubject(selectedSubject);
+
+    if (matchedRoomId == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Subject "$selectedSubject" not found')),
+      );
+      return;
+    }
+
+    final filesPath = await FileManagerService.instance.getFilesPath(
+      matchedRoomId,
+    );
+    final fileName = 'Note_${DateTime.now().millisecondsSinceEpoch}.md';
+    final filePath = path.join(filesPath, fileName);
+
+    final file = File(filePath);
+    await file.writeAsString(markdownText);
+    await FileManagerService.instance.incrementFileCount(matchedRoomId);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Markdown saved to "$selectedSubject"!')),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ...
       appBar: AppBar(
         scrolledUnderElevation: 0,
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-        title: Text("Mark Down Notes", style: TextStyle(fontSize: 20)),
+        title: const Text("Mark Down Notes", style: TextStyle(fontSize: 20)),
         titleSpacing: 0,
-        leading: Builder(
-          builder: (context) {
-            return IconButton(
-              icon: const Icon(
-                Icons.navigate_before,
-                color: Colors.white,
-                size: 40,
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            );
-          },
+        leading: IconButton(
+          icon: const Icon(
+            Icons.navigate_before,
+            color: Colors.white,
+            size: 40,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         actions: [
           IconButton(
-            onPressed: isOnRead
-                ? () async {
-                    setState(() {
-                      isOnRead = false;
-                    });
-                  }
-                : () {
-                    setState(() {
-                      isOnRead = true;
-                    });
-                  },
+            onPressed: _exportMarkdown,
+            icon: const Icon(Icons.file_upload, color: Colors.amber, size: 25),
+          ),
+          IconButton(
+            onPressed: () => setState(() => isOnRead = !isOnRead),
             icon: AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) =>
                   FadeTransition(opacity: animation, child: child),
               child: isOnRead
-                  ? Icon(Icons.edit_note_rounded, color: Colors.amber, size: 30)
-                  : Icon(Icons.remove_red_eye, color: Colors.amber, size: 25),
+                  ? const Icon(
+                      Icons.edit_note_rounded,
+                      color: Colors.amber,
+                      size: 30,
+                    )
+                  : const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.amber,
+                      size: 25,
+                    ),
             ),
           ),
-
           IconButton(
-            onPressed: isSaveEnabled
-                ? () async {
-                    final id = widget.id ?? Uuid().v4();
-                    final title = _titleController.text.trim();
-                    final content = _contentController.text.trim();
-                    final isMD = true;
-
-                    if (widget.id != null) {
-                      await widget.noteManager.updateNote(
-                        id,
-                        title,
-                        content,
-                        isMD,
-                      );
-                    } else {
-                      await widget.noteManager.addNote(
-                        id,
-                        title,
-                        content,
-                        isMD,
-                      );
-                    }
-
-                    Navigator.pop(context);
-                  }
-                : null,
+            onPressed: isSaveEnabled ? _addOrUpdateNote : null,
             icon: AnimatedSwitcher(
               duration: const Duration(milliseconds: 300),
               transitionBuilder: (child, animation) =>
                   FadeTransition(opacity: animation, child: child),
               child: Padding(
-                padding: EdgeInsets.only(right: 15),
+                padding: const EdgeInsets.only(right: 15),
                 child: FaIcon(
                   FontAwesomeIcons.floppyDisk,
-                  key: ValueKey<bool>(isSaveEnabled), // forces switch
+                  key: ValueKey<bool>(isSaveEnabled),
                   color: isSaveEnabled ? Colors.amber : Colors.grey,
                   size: 23,
                 ),
@@ -171,64 +231,44 @@ class _MarkDownEditPageState extends State<MarkDownEditPage> {
           ),
         ],
       ),
-
       body: Padding(
-        padding: EdgeInsets.only(
-          top: 10,
-          left: 10,
-          right: 10,
-          bottom: 20,
-        ), // paddingsss yeyyy
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         child: Column(
           children: [
-            Theme(
-              data: Theme.of(context).copyWith(
-                textSelectionTheme: const TextSelectionThemeData(
-                  cursorColor: Colors.amber,
-                  selectionColor: Color(0x44FFC107),
-                  selectionHandleColor: Colors.amber,
-                ),
-              ),
-              child: isOnRead
-                  ? SizedBox(width: 10)
-                  : TitleField(controller: _titleController, wordLimit: 5),
-            ),
-            if (!isOnRead) Divider(thickness: 1, indent: 10, endIndent: 10),
+            if (!isOnRead)
+              TitleField(controller: _titleController, wordLimit: 5),
+            if (!isOnRead) const Divider(thickness: 1),
             Expanded(
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  textSelectionTheme: const TextSelectionThemeData(
-                    cursorColor: Colors.amber,
-                    selectionColor: Color(0x44FFC107),
-                    selectionHandleColor: Colors.amber,
-                  ),
-                ),
-                child: Expanded(
-                  child: isOnRead
-                      ? MarkdownWidget(
-                          config: MarkdownConfig.darkConfig,
-                          padding: EdgeInsets.all(10),
-                          data: _contentController.text,
-                        )
-                      : Theme(
-                          data: Theme.of(context).copyWith(
-                            textSelectionTheme: const TextSelectionThemeData(
-                              cursorColor: Colors.amber,
-                              selectionColor: Color(0x44FFC107),
-                              selectionHandleColor: Colors.amber,
+              child: isOnRead
+                  ? MarkdownWidget(
+                      config: MarkdownConfig.darkConfig.copy(
+                        configs: [
+                          PreConfig.darkConfig.copy(
+                            wrapper: codeWrapper,
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'SourceCodePro',
                             ),
                           ),
-                          child: SmartMarkdownEditor(
-                            controller: _contentController,
+                          CodeConfig(
+                            style: const TextStyle(
+                              fontFamily: 'SourceCodePro',
+                              fontSize: 14,
+                              backgroundColor: Colors.black12,
+                              color: Colors.tealAccent,
+                            ),
                           ),
-                        ),
-                ),
-              ),
+                          PConfig(textStyle: const TextStyle(fontSize: 14)),
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      data: _contentController.text,
+                    )
+                  : SmartMarkdownEditor(controller: _contentController),
             ),
           ],
         ),
       ),
-
       bottomSheet: !isOnRead
           ? SafeArea(child: MarkdownShortcutBar(controller: _contentController))
           : null,
