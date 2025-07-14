37f2a6dd7c89525b4a9d06ce1740fc13c30abc77
Maruuu1101110 <elijahejrosialda@gmail.com>
2025-07-14 23:05:30 +0800

    refactor: reorganize main initialization logic and improve error handling

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ import 'package:study_forge/tables/note_table.dart';
 import 'package:study_forge/tables/reminder_table.dart';
 import 'package:study_forge/tables/room_table.dart';
 import 'package:study_forge/tables/user_profile_table.dart';
+import 'package:study_forge/pages/settingsPage.dart';
+import 'package:study_forge/utils/ember_ai_service.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 import 'package:study_forge/pages/ember_pages/chat_provider.dart';
@@ -24,34 +26,54 @@ import 'package:study_forge/utils/file_manager_service.dart';
 import 'package:study_forge/pages/homePage.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
 void main() async {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    debugPrint('Caught error: ${details.exception}');
+  };
+  // primary initialization
   WidgetsFlutterBinding.ensureInitialized();
-  tz.initializeTimeZones();
 
-  await FileManagerService.instance.requestStoragePermissions();
-
-  ReminderManager().ensureReminderTableExists();
-  NoteManager().ensureNoteTableExists();
-  RoomTableManager.ensureRoomTableExists();
-  UserProfileManager().ensureUserProfileTableExists();
-
-  final notificationService = NotificationService();
-  await notificationService.initNotif();
-
-  // dotenv
-  await dotenv.load(fileName: "assets/.env");
-
+  // sqlite for desktop
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
   }
 
+  // environment variables | configs
+  await SettingsManager().database;
+  await dotenv.load(fileName: "assets/.env");
+  await EmberAIService().init();
+
+  // timezone initialization
+  tz.initializeTimeZones();
+
+  // storage permissions
+  await FileManagerService.instance.requestStoragePermissions();
+
+  // database initialization
+  await ReminderManager().ensureReminderTableExists();
+  await ReminderManager().ensureNotificationExists();
+  await NoteManager().ensureNoteTableExists();
+  await RoomTableManager.ensureRoomTableExists();
+  await UserProfileManager().ensureUserProfileTableExists();
+
+  // notification service initialization
+  try {
+    if (Platform.isAndroid || Platform.isIOS) {
+      final notificationService = NotificationService();
+      await notificationService.initNotif();
+    }
+  } catch (e) {
+    debugPrint("Notification init failed: $e");
+  }
+
+  // main app run
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => ChatProvider())],
       child: DevicePreview(
-        enabled: !kReleaseMode ? Platform.isLinux : false,
+        enabled: !kReleaseMode && Platform.isLinux,
+        //enabled: true,
         builder: (context) => StudyForge(),
       ),
     ),
